0e41e54885f9218449e498819606ad87974afacb
IANCODE <87940363+ianotee@users.noreply.github.com>
2023-07-08 00:54:44 +0300

    Update settings.py

--- word/settings.py
+++ word/settings.py
@@ -13,13 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-environ.Env.read_env()
+
 
 
 
